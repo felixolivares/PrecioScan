87d356eac5d13f8b12849c19d93888d8ab221dfd
Felix Olivares <felixoe@gmail.com>
2019-08-06 13:11:30 -0600

    Integrated watchkit app

---
+++ WatchPrecioScan Extension/InterfaceController.swift
@@ -0,0 +1,32 @@
+//
+//  InterfaceController.swift
+//  WatchPrecioScan Extension
+//
+//  Created by Félix Olivares on 6/11/19.
+//  Copyright © 2019 Felix Olivares. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ WatchPrecioScan Extension/NotificationController.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationController.swift
+//  WatchPrecioScan Extension
+//
+//  Created by Félix Olivares on 6/11/19.
+//  Copyright © 2019 Felix Olivares. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
